e011a90e20e03597d29a98c2a11fbb8e306732be
unknown <bikrajshrestha2@gmail.com>
2022-12-12 08:12:21 +0545

    small ui changes

--- server/methods/generateImage.js
+++ server/methods/generateImage.js
@@ -1,15 +1,14 @@
 const { Configuration, OpenAIApi } = require('openai');
 const configuration = new Configuration({
-  apiKey: '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',
+  apiKey: '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',
 });
 const openai = new OpenAIApi(configuration);
 
 const generateImage = async (req, res) => {
+  const prompt = req.query.prompt;
 
-    const prompt = req.query.prompt
-    
-    try{
-      const response = await openai.createImage({
+  try {
+    const response = await openai.createImage({
       prompt: prompt,
       n: 1,
       size: '512x512',
@@ -17,12 +16,12 @@ const generateImage = async (req, res) => {
     const iamge_url = response.data.data[0].url;
     console.log(iamge_url);
     res.status(200).json({ image: iamge_url });
-    }catch(e){
-      res.status(400).json({error:e})
-    }
+  } catch (e) {
+    console.log(e.message);
+    res.status(400).json({ error: e });
+  }
 };
 
-
-module.exports ={
-    generateImage
-}
+module.exports = {
+  generateImage,
+};
